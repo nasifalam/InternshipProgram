a9ae2010b34fa6ee5fb93c21322ff47937cfd92a
unknown <nasif.alam@mail.mcgill.ca>
2021-01-17 20:23:24 -0500

    aa

---
+++ laravel/BuildingU/resources/views/usermanagement.blade.php
@@ -0,0 +1,78 @@
+
+@extends('layouts.layout')
+<!DOCTYPE html>
+<html>
+
+
+
+
+
+
+    @php
+
+
+
+
+    @endphp
+
+<h1>User Management Poral</h1>
+
+@foreach($programs as $program)
+
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Program Name</th>
+      <th scope="col">Area</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+<p>
+{{$program['Program Title']}} - {{$program['Program Area']}}
+</p>
+@endforeach
+
+@section('content')
+
+<form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <div class="form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+<br>
+
+@endsection
+
+</html>
