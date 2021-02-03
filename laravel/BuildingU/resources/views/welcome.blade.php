a1c3f01703ad19aaed1b3529996e7c72b6b505b8
unknown <nasif.alam@mail.mcgill.ca>
2021-02-03 01:03:40 -0500

    hu

--- laravel/BuildingU/resources/views/welcome.blade.php
+++ laravel/BuildingU/resources/views/welcome.blade.php
@@ -4,4 +4,5 @@
 <h1> Home Page </h1>
         <a href={{ url('/Programs') }}>Programs</a>
         <a href={{ url('/Programs') }}>Sign in</a>
+        <a href={{ url('/Programs') }}>Sign in</a>
 @endsection
