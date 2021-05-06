e73a1cebaa4dd02dee11830e93903a21ff8c6c37
Boris <borisonekenobi@gmail.com>
2021-05-05 23:28:36 -0400

    Added input fields for age, interests, ethnicity, language, grade

--- laravel/resources/views/student_controller/profile_controller.blade.php
+++ laravel/resources/views/student_controller/profile_controller.blade.php
@@ -12,8 +12,24 @@
 
 
     <div>
-        <h1 class="program_homepage_h1">Profile Page (Changed by Boris)</h1>
-        <input class="test-input">
+        <h1 class="program_homepage_h1">Profile Page</h1>
+        <input name="age" id="age" placeholder="Age">
+        <input name="interests" id="interests" placeholder="Interests">
+
+        <select name="ethnicity" id="ethnicity">
+            <option value="asian">Asian</option>
+            <option value="white">White</option>
+            <option value="black">Black</option>
+            <option value="hispanic">Hispanic</option>
+        </select>
+
+        <select name="language" id="language">
+            <option value="en">English</option>
+            <option value="fr">Français</option>
+            <option value="es">Español</option>
+        </select>
+
+        <input name="grade" id="grade" placeholder="Grade">
     </div>
 
 
