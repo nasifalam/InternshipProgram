eebe92c1932ea4cc403b0eaf1e3f96eaa1d0aac5
Boris <borisonekenobi@gmail.com>
2021-05-07 22:38:20 -0400

    Added css to profile controller form

--- laravel/resources/views/student_controller/profile_controller.blade.php
+++ laravel/resources/views/student_controller/profile_controller.blade.php
@@ -11,28 +11,26 @@
     @endphp
 
 
-    <div>
+    <div class="profile_controller_div">
         <h1 class="program_homepage_h1">Profile Page</h1>
-        <input name="age" id="age" placeholder="Age">
-        <input name="interests" id="interests" placeholder="Interests">
+        <input class="form-control" name="age" id="age" placeholder="Age">
+        <input class="form-control" name="interests" id="interests" placeholder="Interests">
 
-        <select name="ethnicity" id="ethnicity">
+        <select class="form-control" name="ethnicity" id="ethnicity">
             <option value="asian">Asian</option>
             <option value="white">White</option>
             <option value="black">Black</option>
             <option value="hispanic">Hispanic</option>
         </select>
 
-        <select name="language" id="language">
+        <select class="form-control" name="language" id="language">
             <option value="en">English</option>
             <option value="fr">Français</option>
             <option value="es">Español</option>
         </select>
 
-        <input name="grade" id="grade" placeholder="Grade">
-        <!-- Button Variants -->
-        <input name="submit" id="submit" value="Submit" type="button">
-        <button name="submit" id="submit">Submit</button>
+        <input class="form-control" name="grade" id="grade" placeholder="Grade">
+        <button class="btn" name="submit" id="submit">Submit</button>
     </div>
 
 
